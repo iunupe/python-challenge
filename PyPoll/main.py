86389bb1e93fd43cc69c84995a850573529a2fc9
Korbin Mays <Tito@Reginas-MacBook-Pro.local>
2020-03-31 09:00:10 -0500

    Made some changes to PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -30,7 +30,7 @@ winner_count = 0
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-
+    print(csvreader) # prints out an object to read my csv file line-by-line
 
                 # --------------- What's happening here? --------------- #
                 # Thought process is not clicking
@@ -43,22 +43,44 @@ with open(csvpath) as csvfile:
                 # Attempting to work backwards and figure out structure
                 # --------------- Code listed below -------------------- #
 
-    firstrow = next(csvreader)
 
     for row in csvreader:
+        name = row[2]
         total_votes += 1
-        if row[2] in candidates.keys():
-            candidates[row[2]] +=1
+      
+
+        if name in candidates.keys():
+            candidates[name] +=1
+
+
+
+
+# candidates is the dictionary...missing step is adding name to candidate keys, since there should be something, something else
+# google how to add a key to my existing dictionary
+# you can increment a key, but not a string
+
 
         else:
-            candidates[row[2]] +=1
+            candidates[name] = 1
 
-    for key, value in candidates.items():
-        candidates_percentage[key] = round((value/total_votes)*100,2)
+for key, value in candidates.items():
+    candidates_percentage[key] = round((value/total_votes)*100,2)
 
-    for key in candidates.keys():
-        if candidates[key] > winner_count:
-            winner = key
-            winner_count = candidates[key]
+print(candidates_percentage)
 
-    print("Election Results")
+for key in candidates.keys():
+    if candidates[key] > winner_count:
+        winner = key
+        winner_count = candidates[key]
+
+print("Election Results")
+print("")
+print(candidates)
+print("")
+print(winner)
+print("")
+print(candidates.keys())
+print("")
+print(total_votes)
+
+
