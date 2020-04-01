5bbffa94757d4a0f06cf59da2c079e1bbc061e0d
Korbin Mays <Tito@Reginas-MacBook-Pro.local>
2020-04-01 00:54:05 -0500

    Updating HW3 PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,4 +1,4 @@
- # --------------- What's happening here? --------------- #
+                # --------------- What's happening here? --------------- #
                 # Import os module - allows us to create file paths across
                 # operating systems
                 # Import csv module for reading csv files
@@ -25,44 +25,43 @@ winner = ""
 winner_count = 0
 
                 # --------------- What's happening here? --------------- #
-                # The "with open" function is not translating in PyPoll
+                # Opening file using "read" mode; Specifying variable to
+                #  hold contents
                 # --------------- Code listed below -------------------- #
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    print(csvreader) # prints out an object to read my csv file line-by-line
+    #print(csvreader) # prints out an object to read my csv file line-by-
+                     # line
 
                 # --------------- What's happening here? --------------- #
-                # Thought process is not clicking
+                # Reading header row first and printing to screen
                 # --------------- Code listed below -------------------- #
-
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
+    print("")
 
                 # --------------- What's happening here? --------------- #
-                # Attempting to work backwards and figure out structure
+                # Looping through each row of data after the header
                 # --------------- Code listed below -------------------- #
-
-
     for row in csvreader:
         name = row[2]
         total_votes += 1
       
-
         if name in candidates.keys():
             candidates[name] +=1
-
-
-
-
-# candidates is the dictionary...missing step is adding name to candidate keys, since there should be something, something else
-# google how to add a key to my existing dictionary
-# you can increment a key, but not a string
-
-
         else:
             candidates[name] = 1
 
+                # {candidates} is the dictionary...missing step is adding
+                # name to candidate keys, since there should be a [key]
+                # and a [value]
+                # Google how to add a key to my existing dictionary
+                # Note: you can increment a key, but not a string
+
+                # --------------- What's happening here? --------------- #
+                # Looping through each row of data after the header
+                # --------------- Code listed below -------------------- #
 for key, value in candidates.items():
     candidates_percentage[key] = round((value/total_votes)*100,2)
 
@@ -73,14 +72,25 @@ for key in candidates.keys():
         winner = key
         winner_count = candidates[key]
 
+print("")
+
+                # --------------- What's happening here? --------------- #
+                # Printing output/results to screen as a preview
+                # --------------- Code listed below -------------------- #
 print("Election Results")
+print("----------------------------")
+print(f"Total Votes: {total_votes:,}")
+print("----------------------------")
+
+for key, value in candidates.items():
+    print(f"key + : (candidates_percentage:3f) + "%" + {value}")  # FIX THIS LINE!!!
+
+                #print(candidates)
+                #print("")
+print(f"Winner: {winner}")
 print("")
-print(candidates)
-print("")
-print(winner)
-print("")
-print(candidates.keys())
-print("")
-print(total_votes)
+                #print(candidates.keys())
+                #print("")
+
 
 
